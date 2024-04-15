525eb4e220f3b99884080b80c94b36bbd09a9f43
sakhabaevegor <sakhabaevegor@gmail.com>
2024-04-15 13:26:18 +0700

    initial commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StoryKit",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v14)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "StoryKit",
+            targets: ["StoryKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "StoryKit"
+        ),
+        .testTarget(
+            name: "StoryKitTests",
+            dependencies: ["StoryKit"]
+        ),
+    ]
+)
